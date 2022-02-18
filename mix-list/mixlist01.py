174e92a86bcc92a83b25c53ac806fe2fbcc50104
suneku <gtk18@users.noreply.github.com>
2022-02-18 17:44:16 -0500

    mixed lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+my_list = ["192.168.0.5", 5060, "UP"]
+#displays the first item
+print("The first item in the list (IP): " + my_list[0])
+#displays second item
+print("The second item in the list (port): " + str(my_list[1]))
+#displays 3rd item or last item in this case
+print("The last item in the list (state): " + my_list[2])
+
