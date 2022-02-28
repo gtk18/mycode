a4cc0772c8092cc771a2f3e8d8fdf06548b98a62
SunekuMizu <gtk18@users.noreply.github.com>
2022-02-28 00:00:05 -0800

    RPG proj

---
+++ class/classPractice.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 26 13:57:18 2022
+
+@author: gtknapp18
+"""
+
+# Data Types:
+    
+# =============================================================================
+# #Numbers - integers,long, float, complex
+# #String- single quotes, double quotes or triple quotes
+# List- [item,item2,item3]
+# Tuple- created with parenthesis (item,item2,item3)
+# Dictionary - created with braces {key:value,key2:value2}
+# =============================================================================
+
+# Many built-in functions... which means always accessible and no need to import
+# examples include: dict(), dir()str(), type(), etc....
+#print("Howdy all")  #print is the function here, which is a call to pre-written code.
+#Howdy all is the arguement, which is the value that is to be passed to the function.
+
+#shebang line allows others to ID the type of script theyre looking at easily.
+# example of she-bang is #!/usr/bin    or #!/usr/bin/env python3
+
+
+
